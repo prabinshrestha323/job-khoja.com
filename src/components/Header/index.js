d3e9d1dc030f1b2aac9e3cd07ce743dbf2cde14e
prabinshrestha323 <51945844+prabinshrestha323@users.noreply.github.com>
2021-12-21 21:12:00 +0545

    second-job-list

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Box,Grid, Typography, Button } from "@material-ui/core";
+import { Box, Grid, Typography, Button } from "@material-ui/core";
 
-export default props => <Box bgcolor="secondary.main" color="white">
-    <Typography variant ="h5">
-        Open job Listing
-    </Typography>
-    <Button variant ="contained" color="primary" disableElevation>Post a Job </Button>
-</Box>
+export default props =>(
+<Box py={10} bgcolor="secondary.main" color="white">
+<Typography variant ="h5">
+    Open job Listing
+</Typography>
+<Button variant ="contained" color="primary" disableElevation>Post a Job </Button>
+</Box>)
+
