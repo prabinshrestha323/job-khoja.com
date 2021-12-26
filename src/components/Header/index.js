1378c2320ef1d2af1404f06bf16fc72002823802
prabinshrestha323 <51945844+prabinshrestha323@users.noreply.github.com>
2021-12-26 12:28:56 +0545

    second-commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,15 @@ import { Box, Grid, Typography, Button } from "@material-ui/core";
 
 export default props =>(
 <Box py={10} bgcolor="secondary.main" color="white">
-<Typography variant ="h5">
+<Grid container justify="center">
+    <Grid item xs= {10}>
+        <Box display="flex" justifyContent="space-between">
+<Typography variant ="h4">
     Open job Listing
 </Typography>
 <Button variant ="contained" color="primary" disableElevation>Post a Job </Button>
+</Box>
+</Grid>
+</Grid>
 </Box>)
 
